b0db6f866695376a8ab4180724e4799082d51159
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-12 12:25:27 -0500

    writing engineer.test.js, 2 tests passing, 1 test failing

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,25 @@
+const Engineer = require('../lib/Engineer');
+
+// tests creation of Engineer object  
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('', '', '', '');
+    
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(String));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+// test for getGithub()
+test('gets Engineer github', () => {
+    const engineer = new Engineer('Eno', '2', 'eno@gmail.com', 'pwkinsley87');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// test for getRole() 
+test('gets Employee role', () => {
+    const engineer = new Engineer('Eno', '2', 'eno@gmail.com', 'pwkinsley87');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
