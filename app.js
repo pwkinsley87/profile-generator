1d38bbed3236cacd47611cfb654d2842514cfd59
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-12 09:36:05 -0500

    added object creation function for lib files

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+const employees = [];
+
