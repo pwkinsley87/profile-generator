0c442841f5e33f537dc5619d14af46131febcc82
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-14 10:49:14 -0500

    intern.test is passing all tests

---
+++ __tests__/intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require('../lib/intern');
+
+test('Creates an Intern object.', () => {
+    const intern = new Intern('', '', '');
+
+    expect(intern.name).toEqual(expect.any(String));
+    expect(intern.id).toEqual(expect.any(String));
+    expect(intern.email).toEqual(expect.any(String));
+});
+
+test('gets Intern name', () => {
+    const intern = new Intern('Bunk', '5', 'bunkjohnson@gmail.com');
+
+    expect(intern.getName()).toEqual(expect.any(String));
+})
+
+test('gets Intern email', () => {
+    const intern = new Intern('Bunk', '5', 'bunkjohnson@gmail.com');
+
+    expect(intern.getEmail()).toEqual(expect.any(String));
+});
+
+test('gets Intern role', () => {
+    const intern = new Intern('Bunk', '5', 'bunkjohnson@gmail.com');
+
+    expect(intern.getRole()).toEqual('Intern');
+});
